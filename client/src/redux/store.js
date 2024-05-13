d37bce2c943c2ee447f68ea9da85ed199aca8d0f
Vikash-JS <vikash9229sharma@gmail.com>
2024-05-13 21:26:14 +0530

    Added Profile Dropdown

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,25 +1,25 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
+import userReducer from "../redux/user/userSlice";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 
 const rootReducer = combineReducers({
-	uesr: userReducer,
+  user: userReducer,
 });
 
 const persistConfig = {
-	key: "root",
-	storage,
-	version: 1,
+  key: "root",
+  storage,
+  version: 1,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: persistedReducer,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({ serializableCheck: false }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export const persistor = persistStore(store);
