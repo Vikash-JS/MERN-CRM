498af34175570ed69b18436eada7734d1ea58d9c
Vikash-JS <vikash9229sharma@gmail.com>
2024-05-13 22:10:47 +0530

    Private Dashboard for privacy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,26 @@ import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import Header from "./components/Header";
 import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/sign-in" element={<SignIn />} />
-				<Route path="/sign-up" element={<SignUp />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/products" element={<Products />} />
-			</Routes>
-			<FooterCom />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <FooterCom />
+    </BrowserRouter>
+  );
 }
 
 export default App;
